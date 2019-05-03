f1a849c8b246aa7319f189842c1235f85a7723f3
agentboo <zuzi.han@gmail.com>
2019-05-03 08:14:59 -0400

    header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import Peekaboo from "./peekaboo"
+import { Peekaboo } from "./peekaboo"
 
 const Header = ({ siteTitle }) => (
 	<header>
-		<h3>
-			<Link to="/">{siteTitle}</Link>
-		</h3>
+		<nav className="nav-links">
+			<Link to="/" className="site-title">
+				{siteTitle}
+			</Link>
+			<Link to="/about" className="nav-link" activeClassName="nav-link-active">
+				About
+			</Link>
+		</nav>
 		<Peekaboo />
 	</header>
 )
